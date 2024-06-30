6f3fcbefead81d3611996253742b511c107ef148
CainHappyfish <119086094+CainHappyfish@users.noreply.github.com>
2024-06-30 19:04:57 +0800

    update friends

--- packages/vitepress-theme-censored/components/PageFriends.vue
+++ packages/vitepress-theme-censored/components/PageFriends.vue
@@ -15,7 +15,9 @@ import LinkCard from "./global/LinkCard.vue";
     <div class="content scroll-animation">
       <ThemeBanner class="banner"/>
       <div class="about">
-
+        <h1>本站信息</h1>
+        <p>网站名称：破酥的个人博客</p>
+        <p>网站地址：https://cainhappyfish.github.io/vitepress-theme-censored</p>
       </div>
       <div class="friends">
         <LinkCard v-for="(friend, index) in theme.links" :key="index" :link="friend" />
@@ -36,6 +38,15 @@ import LinkCard from "./global/LinkCard.vue";
   display: flex;
   flex-direction: column;
   align-items: center;
+}
+
+.about {
+  margin: 20px 20px;
+  padding: 0 20px;
+  width: 720px;
+  background: var(--censored-nav-color);
+  color: var(--censored-text-color);
+  border-radius: 10px;
 }
 
 .friends {
